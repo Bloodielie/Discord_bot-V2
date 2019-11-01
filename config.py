c246523e79a0b9797810bad5939b4d5bc90cbd29
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2019-11-01 18:43:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+#BOTS
+login_vk = '+375336718846'
+password_vk = 'Kik0kik1235'
+token_bot = 'NjE2MTk5MzQ1ODg0MTAyNjY3.XYtqwQ.eUmRRui7L_ZKY3iqHPWZvjDcTW0'
+prefix = "1"
+
+#Channel
+error_chanel = 605409615785754627
+server_channel = 605409615785754627
+voice_log_channel = 611631555730735114
+voice_check = 612006540043485238
+
+id_dota = 615813505437663242
+id_game = 615816516247617537
+
+#Entertaiments
+F_url = 'https://media2.giphy.com/media/26gR1v0rIDrjSsca4/giphy.gif?cid=790b761160b394b67b35543e8a2425939f3a9b3a622c103c&rid=giphy.gif'
+roll_url = 'https://media2.giphy.com/media/26uf2YTgF5upXUTm0/giphy.gif?cid=790b761105f42d7d6cd52eeed7cd2b5dd6caa0bf2fe0aaca&rid=giphy.gif'
+coin_url = 'https://media.giphy.com/media/1QkVRf2QQ4DCJ7Q115/giphy.gif'
+
+#moderation
+owner_list = [312274114326822936]
+
+#roles
+role_on_member_join = 'Работяги'
+mute_role = 'mute'
