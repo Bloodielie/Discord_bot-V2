23887c31c6f34adbf12364188c7b6710aff11b14
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2019-11-22 22:39:39 +0300

    Add files via upload

--- cogs/monitoring.py
+++ cogs/monitoring.py
@@ -1,4 +1,4 @@
-import discord, psutil, datetime
+import discord, psutil, datetime, platform
 from discord.ext import commands
 
 class Monitoring(commands.Cog):
@@ -10,10 +10,13 @@ class Monitoring(commands.Cog):
         await ctx.send("Собираю данные!!!", delete_after = 2)
         cpu_percent = int(100.0 - psutil.cpu_times_percent(interval=2)[2])
         cpu_count = psutil.cpu_count(logical=True)
+        cpu_name = platform.processor()
         memory_percent = psutil.virtual_memory()[2]
+        memory_full = f"{(str(psutil.virtual_memory()[0])[:2])[0]}.{(str(psutil.virtual_memory()[0])[:2])[1]} Гб"
+        memory_available = f"{(str(psutil.virtual_memory()[1])[:2])[0]}.{(str(psutil.virtual_memory()[1])[:2])[1]} Гб"
         boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%d.%B.%Y %H:%M:%S")
         ping = int(self.bot.latency * 1000)
-        monitor = discord.Embed(title='Мониторинг.', description=f'**Процессор:**\nЗагруженность:`{cpu_percent}%`\nКоличество ядер:`{cpu_count}`\n**Оперативная память:**\nЗагруженность:`{memory_percent}%`\n**Система:**\nUptime:`{boot_time}`', colour= 0xFF0000)
+        monitor = discord.Embed(title='Мониторинг.', description=f'**Процессор:**\nЗагруженность:`{cpu_percent}%`\nКоличество ядер:`{cpu_count}`\nНазвание:`{cpu_name}`\n**Оперативная память:**\nЗагруженность:`{memory_percent}%`\nОбщий объем:`{memory_full}`\nСвободный объем:`{memory_available}`\n**Система:**\nUptime:`{boot_time}`', colour= 0xFF0000)
         monitor.set_author(name = f'{ctx.message.author}', icon_url = ctx.message.author.avatar_url)
         monitor.set_footer(text = f"Пинг:{ping}")
         await ctx.send(embed = monitor, delete_after = 30)
